86b0507ae6797287221eadbd928e9ec0d27c0987
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-29 15:14:59 +0545

    feat: finish up hero section

--- src/views/HomeView/HeroSection.vue
+++ src/views/HomeView/HeroSection.vue
@@ -3,6 +3,12 @@
     <div className="col-1">
       <h1>Play Online Quizzes and Win Prizes Daily!</h1>
       <p>Win upto Rs.5000 monthly!</p>
+      <button class="col-1__register-btn">Register now!</button>
+
+      <div class="col-1__users-div">
+        <span class="col-1__users-title">Number of active users right now</span>
+        <span class="col-1__users-count">20,000+</span>
+      </div>
     </div>
 
     <div>
@@ -24,16 +30,52 @@ import Hero from '@/assets/images/hero.jpg'
   justify-items: end;
   grid-template-columns: repeat(auto-fill, minmax(483px, 1fr));
   justify-content: center;
-  column-gap: 8.5rem;
+  column-gap: 5rem;
 }
 
 .col-1 {
   > * + * {
-    margin-block: 1rem;
+    margin-top: 1.5rem;
   }
 
   p {
     font-size: clamp(1.125rem, 1rem + 0.625vw, 1.563rem);
+  }
+
+  &__register-btn {
+    padding: 1rem 2.5rem;
+    background-color: transparent;
+    color: white;
+    font-size: clamp(1.125rem, 1.089rem + 0.179vw, 1.25rem);
+    border: 1px solid white;
+    border-radius: 4px;
+    transition-duration: $transition-duration;
+
+    &:hover {
+      color: $primary-colour;
+      background-color: white;
+      cursor: pointer;
+    }
+
+    &:active {
+      transform: scale(0.975, 0.975);
+    }
+  }
+
+  &__users-div {
+    @include flex-box($direction: column, $gap: 1rem, $align-items: center, $justify-content: flex-start);
+    text-align: center;
+    width: fit-content;
+  }
+
+  &__users-title {
+    font-size: clamp(1.125rem, 1rem + 0.625vw, 1.5625rem);
+    text-transform: uppercase;
+  }
+
+  &__users-count {
+    font-size: clamp(1.5rem, 0.946rem + 2.768vw, 3.438rem);
+    font-weight: bold;
   }
 }
 </style>
