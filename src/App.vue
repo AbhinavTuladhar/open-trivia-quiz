a680e5eeab832fe34066c5365c37694eab6cfad4
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-29 13:07:11 +0545

    make navbar basis

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HomeView from '@/views/HomeView.vue'
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
   <div class="content-grid">
+    <NavBar />
     <HomeView />
   </div>
 </template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>Navbar body</nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+nav {
+  grid-column: full-width;
+  padding-inline: clamp(1rem, 0.5rem + 2.5vw, 2.75rem);
+  padding-block: 1.5rem;
+  background-color: royalblue;
+  color: white;
+}
+</style>
