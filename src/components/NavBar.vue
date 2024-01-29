3857cb3452aa5dd218a489f05d766608b7941942
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-29 13:36:37 +0545

    feat: make basic navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,48 @@
+<script setup lang="ts">
+const linkData = [
+  { text: 'About', href: '/' },
+  { text: 'Portfolio', href: '/' },
+  { text: 'Services', href: '/' },
+  { text: 'Blog', href: '/' }
+]
+</script>
+
 <template>
-  <nav>Navbar body</nav>
+  <nav>
+    <h1>LOGO</h1>
+
+    <ul>
+      <li v-for="link in linkData" :key="link.text">
+        <a :href="link.href"> {{ link.text }} </a>
+      </li>
+    </ul>
+
+    <button>Play now!</button>
+  </nav>
 </template>
 
-<script setup lang="ts"></script>
-
-<style lang="scss">
+<style lang="scss" scoped>
 nav {
   grid-column: full-width;
-  padding-inline: clamp(1rem, 0.5rem + 2.5vw, 2.75rem);
-  padding-block: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-inline: clamp(1rem, 0.571rem + 2.143vw, 2.5rem);
+  padding-block: 1rem;
   background-color: royalblue;
   color: white;
+
+  ul {
+    display: flex;
+    gap: 2rem;
+  }
+
+  button {
+    padding: 0.5rem 1.5rem;
+    background-color: transparent;
+    color: white;
+    border: 1px solid white;
+    border-radius: 8px;
+  }
 }
 </style>
