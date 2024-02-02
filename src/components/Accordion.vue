65cf0c43385cd10ea3131583198e5e0cb31852bd
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-02-02 13:30:04 +0545

    feat: make navbar responsive

---
+++ src/components/Accordion.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="accordion" :class="{ hide: !isOpen, show: isOpen }">
+    <div class="accordion__children">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+  isOpen: boolean
+}
+
+const props = defineProps<Props>()
+const { isOpen } = toRefs(props)
+</script>
+
+<style scoped lang="scss">
+.accordion {
+  display: grid;
+  transition: all ease-in-out $transition-duration;
+
+  &.hide {
+    grid-template-rows: 0fr;
+    opacity: 0;
+  }
+
+  &.show {
+    grid-template-rows: 1fr;
+    opacity: 1;
+  }
+
+  &__children {
+    overflow: hidden;
+  }
+}
+</style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import Hamburger from '@/assets/images/hamburger.svg'
+import Accordion from '@/components/Accordion.vue'
+
+const isMenuOpen = ref(false)
+
 const linkData = [
   { text: 'About', href: '/' },
   { text: 'Portfolio', href: '/' },
   { text: 'Services', href: '/' },
   { text: 'Blog', href: '/' }
 ]
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <template>
@@ -14,15 +24,32 @@ const linkData = [
         <h2>LOGO</h2>
       </RouterLink>
 
-      <ul>
+      <img :src="Hamburger" class="hamburger" @click="toggleMenu" />
+
+      <!-- For desktop links -->
+      <ul class="link-items desktop-links">
         <li v-for="{ href, text } in linkData" :key="text">
           <RouterLink :to="href"> {{ text }} </RouterLink>
         </li>
       </ul>
 
-      <RouterLink to="/quiz">
+      <RouterLink to="/quiz" class="quiz-button">
         <button>Play now!</button>
       </RouterLink>
+
+      <!-- Mobile links -->
+      <div class="mobile-container" :class="{ 'mobile-container--bordered': isMenuOpen }">
+        <Accordion :is-open="isMenuOpen">
+          <ul class="mobile-links">
+            <li v-for="{ href, text } in linkData" :key="text">
+              <RouterLink :to="href"> {{ text }} </RouterLink>
+            </li>
+          </ul>
+          <RouterLink to="/quiz" class="quiz-button quiz-button--mobile">
+            <button>Play now!</button>
+          </RouterLink>
+        </Accordion>
+      </div>
     </div>
   </nav>
 </template>
@@ -43,24 +70,94 @@ nav {
   align-items: center;
   flex-wrap: wrap;
 
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 2rem;
-  }
-
-  button {
-    padding: 0.5rem 1.5rem;
-    background-color: transparent;
-    color: white;
-    border: 1px solid white;
-    border-radius: 8px;
-    transition-duration: $transition-duration;
+  .hamburger {
+    width: 1.5rem;
+    height: 1.5rem;
+    filter: invert(100%) sepia(0%) saturate(7444%) hue-rotate(39deg) brightness(114%) contrast(111%);
 
     &:hover {
-      color: $surface-100;
-      background-color: white;
       cursor: pointer;
+    }
+
+    @include breakpoint-min(medium) {
+      display: none;
+    }
+  }
+
+  .link-items {
+    display: none;
+
+    @include breakpoint-min(medium) {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 2rem;
+    }
+  }
+
+  .quiz-button {
+    display: none;
+    border: 1px solid white;
+    border-radius: 8px;
+
+    button {
+      padding: 0.5rem 1.5rem;
+      background-color: transparent;
+      color: white;
+      justify-self: start;
+      transition-duration: $transition-duration;
+      border: none;
+      border-radius: 4px;
+
+      &:hover {
+        color: $surface-100;
+        background-color: white;
+        cursor: pointer;
+      }
+    }
+
+    @include breakpoint-min(medium) {
+      display: block;
+    }
+
+    &--mobile {
+      display: flex;
+      justify-content: flex-start;
+      margin-block: 1rem;
+
+      @include breakpoint-min(medium) {
+        display: none;
+      }
+    }
+  }
+}
+
+.mobile-container {
+  $border-colour: silver;
+
+  display: block;
+  width: 100%;
+  margin-top: 1rem;
+
+  &--bordered {
+    border-top: 1px solid $border-colour;
+    border-bottom: 1px solid $border-colour;
+  }
+
+  @include breakpoint-min(medium) {
+    display: none;
+  }
+
+  .mobile-links {
+    border-bottom: 1px solid $border-colour;
+
+    > * {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      padding-block: 0.5rem;
+    }
+    > * + * {
+      border-top: 1px solid $border-colour;
     }
   }
 }
