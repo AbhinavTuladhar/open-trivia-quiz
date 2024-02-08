3f767d9ba94553db3941a7b748fbc44667fca8e8
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-02-08 10:43:56 +0545

    feat: stylise darkmode toggle button
    This uses a 'ball' which goes from left to right and vice-versa
    whenever the lighting mode is changed.

--- src/components/DarkModeSwitch.vue
+++ src/components/DarkModeSwitch.vue
@@ -1,22 +1,57 @@
 <template>
-  <div class="switch" @click="handleClick">Test</div>
+  <div class="switch" @click="handleClick">
+    <div class="switch__circle" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+const isDarkMode = document.documentElement.classList.contains('dark')
+
+const initialMode = isDarkMode ? 'dark' : 'light'
+const mode = ref<typeof initialMode>(initialMode)
+
 const handleClick = () => {
   document.documentElement.classList.toggle('dark')
 }
 </script>
 
 <style scoped lang="scss">
+$switch-width: 56px;
+$switch-height: 28px;
+$ball-radius: 24px;
+$gap: 2px;
+
 .switch {
-  width: 4rem;
-  padding: 0.25rem;
+  position: relative;
+  width: $switch-width;
+  height: $switch-height;
+  background-color: $surface-600;
   border-radius: 100vw;
-  border: 1px solid $surface-300;
+  transition-duration: $transition-duration;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   @include dark-mode {
-    border-color: white;
+    background-color: white;
+  }
+
+  &__circle {
+    position: absolute;
+    top: $gap;
+    right: calc(100% - ($ball-radius + $gap));
+    width: $ball-radius;
+    height: $ball-radius;
+    background-color: white;
+    border-radius: 100%;
+    transition: all ease $transition-duration;
+
+    @include dark-mode {
+      background-color: $surface-100;
+      right: $gap;
+    }
   }
 }
 </style>
